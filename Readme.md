f5648334d586cbed601b51f0048583ba4eaea5d8
Jason Von Busch <jason.vonbusch@nelnet.net>
2019-10-01 09:21:29 -0500

    First commit, initialization

---
+++ Readme.md
@@ -0,0 +1,12 @@
+## Steps to running this project
+1. open up the terminal (docker) and paste the following: ```docker run -p 4222:4222 -ti nats:latest ```
+    * what this does is opens up a docker container and runs it on port 4222, the port that is listening is 3000 though, this is because of a gateway connection.
+2. open up the terminal (bash) and paste the following: ```node index.js```
+    * what this does is start up the application
+3. open up [product list](http://localhost:3000/products) in a new browser window
+    * if json data is returned then the application is functioning properly
+    * other urls that work include (parameters can be different values and do get passed along to perform the requested operation): 
+        * [add operation (alias)](http://localhost:3000/add?a=1&b=2) or [add operation (exact path)](http://localhost:3000/math/add?a=1&b=2)
+        * [subtract operation (alias)](http://localhost:3000/subtract?a=2&b=1) or [subtract operation (exact path)](http://localhost:3000/math/subtract?a=2&b=1)
+        * [multiply operation (alias)](http://localhost:3000/multiply?a=2&b=3) or [multiply operation (exact path)](http://localhost:3000/math/multiply?a=2&b=3)
+        * [divide operation (alias)](http://localhost:3000/divide?a=10&b=5) or [divide operation (exact path)](http://localhost:3000/math/divide?a=10&b=5)
---
+++ index.js
@@ -0,0 +1,77 @@
+const moleculer =  require("moleculer");
+const HTTPServer = require("moleculer-web");
+
+const brokerNode1 = new moleculer.ServiceBroker({
+    nodeID:      "node-1",
+    transporter: "NATS"
+});
+
+brokerNode1.createService({
+    name: "gateway",
+    mixins: [HTTPServer],
+    settings: {
+        routes: [
+            {
+                aliases: {
+                    "GET /products": "products.listProducts",
+                    "GET /add":      "math.add",
+                    "GET /subtract": "math.subtract",
+                    "GET /multiply": "math.multiply",
+                    "GET /divide":   "math.divide",
+                }
+            }
+        ]
+    }
+});
+
+const brokerNode2 = new moleculer.ServiceBroker({
+    nodeID:      "node-2",
+    transporter: "NATS"
+})
+
+brokerNode2.createService({
+    name: "products",
+    actions: {
+        listProducts(ctx) {
+            return [
+                {name: "Apples",  price: 5},
+                {name: "Oranges", price: 3},
+                {name: "Bananas", price: 2}
+            ];
+        }
+    },
+});
+
+const brokerNode3 = new moleculer.ServiceBroker({
+    nodeID:      "node-3",
+    transporter: "NATS"
+});
+
+brokerNode3.createService({
+    name: "math",
+    actions: {
+        add(ctx) {      //http://localhost:3000/math/add?a=1&b=2      or use the alias: http://localhost:3000/add?a=1&b=2
+            return Number(ctx.params.a) + Number(ctx.params.b);
+        },
+        subtract(ctx) { //http://localhost:3000/math/subtract?a=2&b=1 or use the alias: http://localhost:3000/subtract?a=2&b=1
+            return Number(ctx.params.a) - Number(ctx.params.b);
+        },
+        multiply(ctx) { //http://localhost:3000/math/multiply?a=2&b=3 or use the alias: http://localhost:3000/multiply?a=2&b=3
+            return Number(ctx.params.a) * Number(ctx.params.b);
+        },
+        divide(ctx) {   //http://localhost:3000/math/divide?a=10&b=5  or use the alias: http://localhost:3000/divide?a=10&b=5
+            return Number(ctx.params.a) / Number(ctx.params.b);
+        }
+    },
+});
+
+Promise.all(brokerNode1.start()
+          , brokerNode2.start()
+          , brokerNode3.start().then(() => brokerNode3.call("math.add", {a: 1, b: 2}))
+                               .then(res => console.log("1 + 2 =", res))
+                               .then(() => brokerNode3.call("math.subtract", {a: 2, b: 1}))
+                               .then(res => console.log("2 - 1 =", res))
+                               .then(() => brokerNode3.call("math.multiply", {a: 2, b: 3}))
+                               .then(res => console.log("2 * 3 =", res))
+                               .then(() => brokerNode3.call("math.divide", {a: 10, b: 5}))
+                               .then(res => console.log("10 / 5 =", res)));
